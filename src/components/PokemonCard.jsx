23a631776deefb7e9e9f33d7caabfe2a132e8708
Thierry Vicente <119328995+StalkerVince@users.noreply.github.com>
2023-10-11 10:30:03 +0200

    Proptypes

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,16 +1,24 @@
-function PokemonCard(props) {
-  console.log(props);
+import PropTypes from "prop-types";
+
+function PokemonCard({ pokemon }) {
+  const { name, imgSrc } = pokemon;
+
+  PokemonCard.propTypes = {
+    PokemonCard:
+      PropTypes.shape[
+        {
+          name: PropTypes.string.isRequired,
+          imgSrc: PropTypes.string,
+        }
+      ],
+  };
 
   return (
     <div>
       <figure>
         {/*Si pokemon.imgSrc, alors mettre image sinon afficher ??? */}
-        {props.pokemon.imgSrc ? (
-          <img src={props.pokemon.imgSrc}></img>
-        ) : (
-          <p>???</p>
-        )}
-        <figcaption>{props.pokemon.name}</figcaption>
+        {imgSrc ? <img src={imgSrc}></img> : <p>???</p>}
+        <figcaption>{name}</figcaption>
       </figure>
     </div>
   );
